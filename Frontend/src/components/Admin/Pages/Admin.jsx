02f67c21f9679414dfb196f16d3285b386e27dff
Aniket Kole <aniketkole07@gmail.com>
2026-01-13 11:05:33 +0530

    This page inlcude full admin dashboard

---
+++ Frontend/src/components/Admin/Pages/Admin.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Sidebar from '../Sidebar/Sidebar'
+import Dashboard from "../Dashboard/Dashboard"
+import Projects from '../Projects/Projects'
+
+const Admin = () => {
+
+  const currentTab = useSelector((state) => state.currentTab.value)
+
+  switch (currentTab) {
+    case "dashboard": {
+      return (
+        <main className='grid gap-4 p-4 grid-cols-[210px_1fr]'>
+          <Sidebar />
+          <Dashboard />
+        </main>
+      )
+    }
+      break;
+
+    case "projects": {
+      return (
+        <main className='grid gap-4 p-4 grid-cols-[210px_1fr]'>
+          <Sidebar />
+          <Projects />
+        </main>
+      )
+    }
+      break;
+
+    case "team": {
+      return (
+        <main className='grid gap-4 p-4 grid-cols-[210px_1fr]'>
+          <Sidebar />
+          <Dashboard />
+        </main>
+      )
+    }
+      break;
+
+    case "todo's": {
+      return (
+        <main className='grid gap-4 p-4 grid-cols-[210px_1fr]'>
+          <Sidebar />
+          <Dashboard />
+        </main>
+      )
+    }
+      break;
+
+    default: return (<> h3llo</>)
+      break;
+  }
+
+
+}
+
+export default Admin
