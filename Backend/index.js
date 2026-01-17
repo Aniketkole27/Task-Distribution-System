d937f74948235cbd25e95804339153d860eb6b25
Aniket Kole <aniketkole07@gmail.com>
2026-01-17 12:04:55 +0530

    Add package.json for backend setup with dependencies and scripts

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+import express from 'express'
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 8000
+
+app.get("/", (req, res) => {
+    res.send("<h1>Next handle kar, Darsha</h1>")
+})
+
+app.listen(port, () => {
+    console.log("Sever listening on port 8000")
+})
