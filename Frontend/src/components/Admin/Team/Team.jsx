ba76d653d83643bc36be3395f3f3e0ab35933713
Aniket Kole <aniketkole07@gmail.com>
2026-01-18 13:45:57 +0530

    add EditMember component and toggle functionality in Team component

--- Frontend/src/components/Admin/Team/Team.jsx
+++ Frontend/src/components/Admin/Team/Team.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Greeting from '../Dashboard/Greeting'
 import TopSection from './TopSection'
 import TeamList from './TeamList'
+import EditMember from './EditMember'
 
 function Team() {
+  const [open, setOpen] = useState(false)
   return (
     <div className='bg-white text-black rounded-lg pb-3 shadow h-full'>
-       <Greeting />
-       <TopSection />
-       <TeamList />
+      <Greeting />
+      <TopSection setOpen={setOpen} />
+      {
+        open ? <EditMember setOpen={setOpen} /> : null
+      }
+      <TeamList />
     </div>
   )
 }
