82aaa448c47eeaf445168b2eb11845e94455bd71
Aniket Kole <aniketkole07@gmail.com>
2026-01-18 18:02:06 +0530

    fix: clean up .gitignore and resolve merge conflicts in package-lock.json and EditMember.jsx

--- Frontend/src/components/Admin/Team/EditMember.jsx
+++ Frontend/src/components/Admin/Team/EditMember.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const EditMember = ({setOpen }) => {
+const EditMember = ({ setOpen }) => {
     return (
         <div
             className='fixed inset-0 bg-stone-950/50 border z-50 flex items-start justify-center'
@@ -10,25 +10,8 @@ const EditMember = ({setOpen }) => {
                 onClick={(e) => e.stopPropagation()}
                 className='bg-white w-full max-w-2xl mx-auto mt-10 min-h-[80vh] rounded'
             >
-<<<<<<< HEAD
-                {/* {
-=======
-                {
->>>>>>> auth-feature
-                    teamMember.map((member) => (
-                        <div key={email}>
-                            <h1>{member.name}</h1>
-                            <h1>{member.email}</h1>
-                            <h1>{member.mobile}</h1>
-                            <h1>{member.isActive}</h1>
-                        </div>
-                    ))
-<<<<<<< HEAD
-                } */}
+
                 <h1>Edit Members</h1>
-=======
-                }
->>>>>>> auth-feature
             </div>
         </div>
     )
